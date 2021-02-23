c765bb3fae5c28f1d18769725d8d7770c0b89eb4
Scott Bender <scott@scottbender.net>
2021-02-23 13:47:43 -0500

    feature: add ability to get device status from mdns

--- src/classes/ChangeStateZeroconf.js
+++ src/classes/ChangeStateZeroconf.js
@@ -29,6 +29,31 @@ class ChangeStateZeroconf extends WebSocket {
 
     return { status: 'ok', state };
   }
+  static async get({ url, device, switches }) {
+    const selfApikey = device.apikey;
+    const deviceId = device.deviceid;
+    const deviceKey = device.devicekey;
+
+    //const endpoint = switches ? 'switches' : 'switch';
+    const endpoint = 'info'
+
+    const body = zeroConfUpdatePayload(selfApikey, deviceId, deviceKey, {});
+
+    const request = await fetch(`${url}/${endpoint}`, {
+      method: 'post',
+      body: JSON.stringify(body),
+    });
+
+    const response = await request.json();
+
+    const error = _get(response, 'error', false);
+
+    if (error === 403) {
+      return { error, msg: response.reason };
+    }
+
+    return { status: 'ok', state };
+  }
 }
 
 module.exports = ChangeStateZeroconf;
